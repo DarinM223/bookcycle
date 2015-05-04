3e42d449fe85ee6c17393c924f21e1455270cf0a
Darin Minamoto <darinm223@gmail.com>
2015-05-04 03:48:10 -0700

    Book detail shows up

--- main.go
+++ main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 	"html/template"
 	"net/http"
 )
 
 func main() {
+	db, err := gorm.Open("sqlite3", "./sqlite_file.db")
+	_ = db
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	fs := http.FileServer(http.Dir("./static"))
 	r := mux.NewRouter()
 	// ./static/css/main.css maps to
 	// localhost:blah/public/css/main.css
-	r.Handle("/public", fs)
+	http.Handle("/public/", fs)
 	r.HandleFunc("/", RootHandler)
+	r.Methods("GET", "DELETE").Path("/books/{id}").HandlerFunc(BookHandler)
 
 	fmt.Println("Listening...")
 	http.ListenAndServe(":8080", r)
@@ -26,4 +36,25 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	t.Execute(w, nil)
+}
+
+func BookHandler(w http.ResponseWriter, r *http.Request) {
+	book_id := mux.Vars(r)["id"]
+	fmt.Println(book_id)
+	test_book := Book{
+		Title:     "Sample book",
+		Author:    "Sample author",
+		Class:     "Sample class",
+		Professor: "Sample professor",
+		Version:   "Sample version",
+		Price:     "Sample price",
+		Condition: "Sample condition",
+	}
+
+	t, err := template.ParseFiles("templates/book_detail.html")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	t.Execute(w, test_book)
 }
---
+++ types.go
@@ -0,0 +1,20 @@
+package main
+
+type User struct {
+	Id     int     `sql:"AUTO_INCREMENT"`
+	Rating float64 `sql:"not null" sql:"default:0"`
+	Email  string  `sql:"not null"`
+	Phone  int
+}
+
+type Book struct {
+	Id        int    `sql:"AUTO_INCREMENT"`
+	Title     string `sql:"not null"`
+	Author    string `sql:"not null"`
+	Class     string `sql:"not null"`
+	Professor string `sql:"not null"`
+	Version   string `sql:"not null"`
+	Price     string `sql:"not null"`
+	Condition string `sql:"not null"`
+	UserId    int    `sql:"index"`
+}
