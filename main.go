6c00cdf6fe3490387d3f51499cb9324e2af6cf90
Darin Minamoto <darinm223@gmail.com>
2015-05-04 02:27:04 -0700

    Got first page to show up

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	// ./static/css/main.css maps to
-	// localhost:blah/css/main.css
-	http.Handle("/", fs)
+	// localhost:blah/public/css/main.css
+	http.Handle("/public", fs)
+	http.HandleFunc("/", RootHandler)
+
 	fmt.Println("Listening...")
 	http.ListenAndServe(":8080", nil)
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	t.Execute(w, nil)
 }
