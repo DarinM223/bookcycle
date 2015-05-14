4d0aa459473d6f7f2e35154486915e9788ed880e
Darin Minamoto <darinm223@gmail.com>
2015-05-14 15:12:32 -0700

    Refactored out user testing utilities into class

---
+++ testing.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+)
+
+func SetUpTesting() (*httptest.Server, gorm.DB) {
+	// Set up database
+	db, _ := gorm.Open("sqlite3", "./sqlite_file_test.db")
+	db.LogMode(false)
+	db.DropTable(&User{})
+	db.DropTable(&Book{})
+	db.DropTable(&Book{})
+	db.AutoMigrate(&User{}, &Book{}, &Message{})
+
+	// set up test db
+	server := httptest.NewServer(Routes(db))
+
+	return server, db
+}
+
+type UserTesting struct {
+	DB     gorm.DB
+	Server *httptest.Server
+}
+
+func NewUserTesting() UserTesting {
+	server, db := SetUpTesting()
+	return UserTesting{
+		DB:     db,
+		Server: server,
+	}
+}
+
+func (n UserTesting) NewUserUrl() string {
+	return fmt.Sprintf("%s/users/new", n.Server.URL)
+}
+
+func (n UserTesting) EditUserUrl() string {
+	return fmt.Sprintf("%s/users/edit", n.Server.URL)
+}
+
+func (n UserTesting) LoginUserUrl() string {
+	return fmt.Sprintf("%s/login", n.Server.URL)
+}
+
+func (n UserTesting) ViewUserUrl(id int) string {
+	return fmt.Sprintf("%s/users/%d", n.Server.URL, id)
+}
+
+func (n UserTesting) MakeTestUser(u User, password string, password_confirm string) error {
+	userJson := url.Values{}
+	userJson.Set("first_name", u.Firstname)
+	userJson.Set("last_name", u.Lastname)
+	userJson.Set("email", u.Email)
+	userJson.Set("phone", strconv.Itoa(u.Phone))
+	userJson.Set("password1", password)
+	userJson.Set("password2", password_confirm)
+
+	request, err := http.NewRequest("POST", n.NewUserUrl(), bytes.NewBufferString(userJson.Encode()))
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	// Test that POST request returns success
+	if res.StatusCode != 200 {
+		//body, _ := ioutil.ReadAll(res.Body)
+		//fmt.Println(string(body))
+		return errors.New("POST Success should be 200")
+	}
+
+	return nil
+}
+
+func (n UserTesting) EditTestUser(u User, c *http.Cookie, password string, password_confirm string) error {
+	userJson := url.Values{}
+	userJson.Set("first_name", u.Firstname)
+	userJson.Set("last_name", u.Lastname)
+	userJson.Set("email", u.Email)
+	userJson.Set("phone", strconv.Itoa(u.Phone))
+	userJson.Set("password1", password)
+	userJson.Set("password2", password_confirm)
+
+	request, err := http.NewRequest("POST", n.EditUserUrl(), bytes.NewBufferString(userJson.Encode()))
+	request.AddCookie(c)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	// Test that POST request returns success
+	if res.StatusCode != 200 {
+		//body, _ := ioutil.ReadAll(res.Body)
+		//fmt.Println(string(body))
+		return errors.New("POST Success should be 200")
+	}
+
+	return nil
+}
+
+func (n UserTesting) LoginUser(email string, password string) (*http.Cookie, error) {
+	loginJson := url.Values{}
+	loginJson.Set("email", email)
+	loginJson.Set("password", password)
+
+	request, err := http.NewRequest("POST", n.LoginUserUrl(), bytes.NewBufferString(loginJson.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	transport := http.Transport{}
+	res, err := transport.RoundTrip(request)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, cookie := range res.Cookies() {
+		if cookie.Name == "bookcycle" {
+			return cookie, nil
+		}
+	}
+
+	return nil, errors.New("Cookie not set")
+}
--- users_test.go
+++ users_test.go
@@ -1,133 +1,19 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
-	"errors"
-	"fmt"
-	"github.com/jinzhu/gorm"
 	_ "io/ioutil"
 	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"strconv"
 	"testing"
 )
 
-var (
-	db           gorm.DB
-	server       *httptest.Server
-	newUserUrl   string
-	editUserUrl  string
-	loginUserUrl string
-)
+var userTesting UserTesting
 
 func init() {
-	// Set up database
-	db, _ = gorm.Open("sqlite3", "./sqlite_file_test.db")
-	db.LogMode(false)
-	db.DropTable(&User{})
-	db.DropTable(&Book{})
-	db.DropTable(&Book{})
-	db.AutoMigrate(&User{}, &Book{}, &Message{})
-
-	// set up test db
-	server = httptest.NewServer(Routes(db))
-
-	newUserUrl = fmt.Sprintf("%s/users/new", server.URL)
-	editUserUrl = fmt.Sprintf("%s/users/edit", server.URL)
-	loginUserUrl = fmt.Sprintf("%s/login", server.URL)
-}
-
-func makeTestUser(u User, password string, password_confirm string) error {
-	userJson := url.Values{}
-	userJson.Set("first_name", u.Firstname)
-	userJson.Set("last_name", u.Lastname)
-	userJson.Set("email", u.Email)
-	userJson.Set("phone", strconv.Itoa(u.Phone))
-	userJson.Set("password1", password)
-	userJson.Set("password2", password_confirm)
-
-	request, err := http.NewRequest("POST", newUserUrl, bytes.NewBufferString(userJson.Encode()))
-	if err != nil {
-		return err
-	}
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	res, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return err
-	}
-
-	// Test that POST request returns success
-	if res.StatusCode != 200 {
-		//body, _ := ioutil.ReadAll(res.Body)
-		//fmt.Println(string(body))
-		return errors.New("POST Success should be 200")
-	}
-
-	return nil
-}
-
-func editTestUser(u User, c *http.Cookie, password string, password_confirm string) error {
-	userJson := url.Values{}
-	userJson.Set("first_name", u.Firstname)
-	userJson.Set("last_name", u.Lastname)
-	userJson.Set("email", u.Email)
-	userJson.Set("phone", strconv.Itoa(u.Phone))
-	userJson.Set("password1", password)
-	userJson.Set("password2", password_confirm)
-
-	request, err := http.NewRequest("POST", editUserUrl, bytes.NewBufferString(userJson.Encode()))
-	request.AddCookie(c)
-	if err != nil {
-		return err
-	}
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	res, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return err
-	}
-
-	// Test that POST request returns success
-	if res.StatusCode != 200 {
-		//body, _ := ioutil.ReadAll(res.Body)
-		//fmt.Println(string(body))
-		return errors.New("POST Success should be 200")
-	}
-
-	return nil
-}
-
-func loginUser(email string, password string) (*http.Cookie, error) {
-	loginJson := url.Values{}
-	loginJson.Set("email", email)
-	loginJson.Set("password", password)
-
-	request, err := http.NewRequest("POST", loginUserUrl, bytes.NewBufferString(loginJson.Encode()))
-	if err != nil {
-		return nil, err
-	}
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	transport := http.Transport{}
-	res, err := transport.RoundTrip(request)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, cookie := range res.Cookies() {
-		if cookie.Name == "bookcycle" {
-			return cookie, nil
-		}
-	}
-
-	return nil, errors.New("Cookie not set")
+	userTesting = NewUserTesting()
 }
 
 func TestCreateUser(t *testing.T) {
-	request, err := http.NewRequest("GET", newUserUrl, nil)
+	request, err := http.NewRequest("GET", userTesting.NewUserUrl(), nil)
 	if err != nil {
 		t.Error(err)
 		return
@@ -153,21 +39,21 @@ func TestCreateUser(t *testing.T) {
 	}
 
 	// Test that creating User with no password has error
-	err = makeTestUser(test_user, "", "")
+	err = userTesting.MakeTestUser(test_user, "", "")
 	if err == nil {
 		t.Error("Creating User with no password should return error")
 		return
 	}
 
 	// Test that creating User with different passwords has error
-	err = makeTestUser(test_user, "password1", "password2")
+	err = userTesting.MakeTestUser(test_user, "password1", "password2")
 	if err == nil {
 		t.Error("Creating User with wrong passwords should return error")
 		return
 	}
 
 	// Test that creating User properly returns success
-	err = makeTestUser(test_user, "password", "password")
+	err = userTesting.MakeTestUser(test_user, "password", "password")
 	if err != nil {
 		t.Error(err)
 		return
@@ -175,7 +61,7 @@ func TestCreateUser(t *testing.T) {
 
 	// Test if user is created and proper fields are created
 	var users []User
-	db.Preload("users").Find(&users)
+	userTesting.DB.Preload("users").Find(&users)
 	if len(users) != 1 {
 		t.Errorf("Users length should be 1, instead: %d", len(users))
 		return
@@ -202,11 +88,7 @@ func TestCreateUser(t *testing.T) {
 		return
 	}
 
-	db.Delete(&users[0])
-}
-
-func encode(src []byte) string {
-	return base64.URLEncoding.EncodeToString(src)
+	userTesting.DB.Delete(&users[0])
 }
 
 func TestEditUser(t *testing.T) {
@@ -216,13 +98,13 @@ func TestEditUser(t *testing.T) {
 		Email:     "testuser@gmail.com",
 		Phone:     123456789,
 	}
-	err := makeTestUser(test_user, "password", "password")
+	err := userTesting.MakeTestUser(test_user, "password", "password")
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	request, err := http.NewRequest("GET", editUserUrl, nil)
+	request, err := http.NewRequest("GET", userTesting.EditUserUrl(), nil)
 	if err != nil {
 		t.Error(err)
 		return
@@ -240,14 +122,14 @@ func TestEditUser(t *testing.T) {
 		return
 	}
 
-	loginCookie, err := loginUser(test_user.Email, "password")
+	loginCookie, err := userTesting.LoginUser(test_user.Email, "password")
 	if err != nil {
 		t.Error(err)
 		t.Error("Error logging in user")
 		return
 	}
 
-	request, err = http.NewRequest("GET", editUserUrl, nil)
+	request, err = http.NewRequest("GET", userTesting.EditUserUrl(), nil)
 	request.AddCookie(loginCookie) // add login session cookie
 	if err != nil {
 		t.Error(err)
@@ -275,35 +157,35 @@ func TestEditUser(t *testing.T) {
 	}
 
 	// sending edit request with different password and password confirmation should have error
-	err = editTestUser(edited_user, loginCookie, "password1", "password2")
+	err = userTesting.EditTestUser(edited_user, loginCookie, "password1", "password2")
 	if err == nil {
 		t.Error("Editing user with wrong passwords should have error")
 		return
 	}
 
 	// sending edit request without password should not change password
-	err = editTestUser(edited_user, loginCookie, "", "")
+	err = userTesting.EditTestUser(edited_user, loginCookie, "", "")
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
 	var user User
-	db.Where("email LIKE ?", "tu@gmail.com").First(&user)
+	userTesting.DB.Where("email LIKE ?", "tu@gmail.com").First(&user)
 	if !user.Validate("password") {
 		t.Error("Password should not have changed")
 		return
 	}
 
 	// sending edit request with a different password should change password
-	err = editTestUser(edited_user, loginCookie, "another_password", "another_password")
-	db.Where("email LIKE ?", "tu@gmail.com").First(&user)
+	err = userTesting.EditTestUser(edited_user, loginCookie, "another_password", "another_password")
+	userTesting.DB.Where("email LIKE ?", "tu@gmail.com").First(&user)
 	if !user.Validate("another_password") {
 		t.Error("Password should have changed")
 		return
 	}
 
-	db.Delete(&user)
+	userTesting.DB.Delete(&user)
 }
 
 func TestViewUser(t *testing.T) {
@@ -313,7 +195,7 @@ func TestViewUser(t *testing.T) {
 		Email:     "testuser@gmail.com",
 		Phone:     123456789,
 	}
-	user_route := fmt.Sprintf("%s/users/1", server.URL)
+	user_route := userTesting.ViewUserUrl(1)
 
 	// Test that accessing GET for nonexisting user returns 404
 	request, err := http.NewRequest("GET", user_route, nil)
@@ -328,7 +210,7 @@ func TestViewUser(t *testing.T) {
 	}
 
 	// Test that accessing GET for existing user returns success
-	err = makeTestUser(test_user, "password", "password")
+	err = userTesting.MakeTestUser(test_user, "password", "password")
 	if err != nil {
 		t.Error(err)
 		return
