32ae6fcd2e1b13ac1e9b0033eda55fdbd3f14816
Han Lee <fliplayer@gmail.com>
2015-05-27 21:29:54 -0700

    modified book details

---
+++ static/js/book_details.js
@@ -0,0 +1,46 @@
+  function getISBN(isbn, callback) {
+    if (isbn.trim().length === 0) {
+      return callback(Error('ISBN field is empty'), null);
+    }
+    $.ajax({
+      method: 'GET',
+      url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + encodeURIComponent(isbn)
+    }).success(function(data) {
+      if (data.totalItems === 0) {
+        callback(new Error('ISBN is invalid'), null);
+      } else if (data.totalItems > 1) {
+        callback(new Error('ISBN is ambiguous'), null);
+      } else {
+        callback(null, data.items[0]);
+      }
+    });
+  }
+
+function DisplayBook(isbn) {
+	$(document).ready(function() {
+
+
+    getISBN(isbn, function(err, data) {
+      if (err) {
+        alert(err.message);
+        return;
+      }
+    $('.author').text('Author(s): ' + data.volumeInfo.authors.join(', '));
+
+    if (typeof(data.volumeInfo.imageLinks) === 'undefined' || 
+            data.volumeInfo.imageLinks === null || 
+            typeof(data.volumeInfo.imageLinks.thumbnail) === 'undefined' ||
+            data.volumeInfo.imageLinks.thumbnail === null) {
+
+          $('.bookpicture').attr('src', '/images/no_image.png');
+        } else {
+          $('.bookpicture').attr('src', data.volumeInfo.imageLinks.thumbnail);
+        }
+
+
+  	});
+
+
+
+	});
+}
