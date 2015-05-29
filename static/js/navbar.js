43bcbcfdd4b3faa73758ff9b0559f06ada15799c
Darin Minamoto <darinm223@gmail.com>
2015-05-28 20:41:22 -0700

    Fixed message dropdown randomly moving around

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -12,64 +12,78 @@ $(document).click(function() {
 });
 // console.log(senderIdList);
 
+
+jQuery.fn.insertAt = function(index, element) {
+  var lastIndex = this.children().size()
+  if (index < 0) {
+    index = Math.max(0, lastIndex + 1 + index)
+  }
+  this.append(element)
+  if (index < lastIndex) {
+    this.children().eq(index).before(this.children().last())
+  }
+  return this;
+}
+
 function isInArray(value, array) {
   return array.indexOf(value) > -1;
 }
+
 setInterval(function() {
-	// senderIdList = [];
+  // senderIdList = [];
   // console.log('Sending AJAX');
-$.ajax({
-  type: 'GET',
-  url: '/messages'
-}).success(function(data, textStatus, jqXHR) {
-  $('.msg').remove();
-  var messageNum = 0;
-  var msgCounter = 0;
-  var senderIdList = [];
-  var message = [];
-  var senderName;
-  var read = false;
-  
-  for(var i = 0; i < data.length; i++) {
-    // console.log(($.inArray(data[i]['senderId'], senderIdList)));
-    if (($.inArray(data[i]['senderId'], senderIdList)) == -1) {
-    	// console.log("here");
-      if((data[i]['message']).length > 60) {
-        message.push((data[i]['message']).substring(0,60) + "...");
-      }
-      else {
-        message.push(data[i]['message']);
-      }
-      read = data[i]['read'];
-      senderIdList.push(data[i]['senderId']);
-      (function(messageNum, read) {
-        $.ajax({
-          type: 'GET',
-          url: '/users/' + data[i]['senderId'] + '/json'
-        }).success(function(data, textStatus, jqXHR) {
-          senderName = data['first_name'] + " " + data['last_name'];
-          // console.log(senderName);
-          if(read) {
-            $('#notificationsBody').append('<a class="msg" href="/message/' + senderIdList[messageNum] +'"><div class="readmsg"><span id="sendername">' + senderName + '</span><br><br><span id="msgpreview">' + message[messageNum] +'</span></div></a>');
-          }
-          else {
-            $('#notificationsBody').append('<a class="msg" href="/message/' + senderIdList[messageNum] +'"><div class="unreadmsg"><span id="sendername">' + senderName + '</span><br><br><span id="msgpreview">' + message[messageNum] +'</span></div></a>');
-          }
-        });
-      })(messageNum, read);
-      messageNum++;
-      if(!read) {
-        msgCounter++;
+  $.ajax({
+    type: 'GET',
+    url: '/messages'
+  }).success(function(data, textStatus, jqXHR) {
+    $('.msg').remove();
+    var messageNum = 0;
+    var msgCounter = 0;
+    var senderIdList = [];
+    var message = [];
+    var senderName;
+    var read = false;
+
+    for(var i = 0; i < data.length; i++) {
+      // console.log(($.inArray(data[i]['senderId'], senderIdList)));
+      if (($.inArray(data[i]['senderId'], senderIdList)) == -1) {
+        // console.log("here");
+        if((data[i]['message']).length > 60) {
+          message.push((data[i]['message']).substring(0,60) + "...");
+        }
+        else {
+          message.push(data[i]['message']);
+        }
+        read = data[i]['read'];
+        senderIdList.push(data[i]['senderId']);
+        (function(messageNum, read) {
+          $.ajax({
+            type: 'GET',
+            url: '/users/' + data[i]['senderId'] + '/json'
+          }).success(function(data, textStatus, jqXHR) {
+            senderName = data['first_name'] + " " + data['last_name'];
+            // console.log(senderName);
+            if(read) {
+              $('#notificationsBody').insertAt(messageNum, '<a class="msg" href="/message/' + senderIdList[messageNum] +'"><div class="readmsg"><span id="sendername">' + senderName + '</span><br><br><span id="msgpreview">' + message[messageNum] +'</span></div></a>');
+            }
+            else {
+              $('#notificationsBody').insertAt(messageNum, '<a class="msg" href="/message/' + senderIdList[messageNum] +'"><div class="unreadmsg"><span id="sendername">' + senderName + '</span><br><br><span id="msgpreview">' + message[messageNum] +'</span></div></a>');
+            }
+          });
+        })(messageNum, read);
+        messageNum++;
+        if(!read) {
+          msgCounter++;
+        }
       }
     }
-  }
-  // console.log(msgCounter);
-  if (msgCounter != 0) {
-    $('.messages').append('<span id="notification_count">' + msgCounter + '</span>');
-  }
-}).error(function(jqXHR, textStatus, err) {
-  console.log(err);
-});
+    // console.log(msgCounter);
+    if (msgCounter != 0) {
+      $('.messages').append('<span id="notification_count">' + msgCounter + '</span>');
+    }
+  }).error(function(jqXHR, textStatus, err) {
+    console.log(err);
+  });
 
 }, 1000);
 
